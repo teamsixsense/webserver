3c26d2baeb06aefc34d63d7efd6fe9428986e6ea
GrolarKim <audwnswmf@naver.com>
2022-02-22 21:12:37 +0900

    url 기초 연결 및 모델 기초 생성

--- nst/models.py
+++ nst/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Picmodel(models.Model):
+    class Meta:
+        db_table = "pictures"
+
+    image = models.ImageField()
+    name = models.CharField(null=True)
+    artist = models.CharField(null=True)
+    desc = models.TextField(null=True)
+    nation = models.CharField()
