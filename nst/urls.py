6db7a3862070bd58636202f0c09418d35c5a1b81
GrolarKim <audwnswmf@naver.com>
2022-02-24 18:03:27 +0900

    실험했던거 지움

--- nst/urls.py
+++ nst/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path("category/<str:nation>", views.PicListView.as_view(), name="list"),
     path("detail/<int:pk>", views.PicDetailView.as_view(), name="detail"),
     path("result/<int:result>", views.ResultView.as_view(), name="result"),
-    path("result2/<int:result>", views.ResultViewTwo, name="resulttwo"),
 ]
